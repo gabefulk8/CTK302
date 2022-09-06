66ba49c7ef82cf898383dfa6e057e097dcfeae1f
gabefulk8 <89150841+gabefulk8@users.noreply.github.com>
2022-09-06 17:30:03 -0500

    fonts project

--- Unit1/fonts/sketch.js
+++ Unit1/fonts/sketch.js
@@ -1,7 +1,20 @@
+let f1, f2, f3;
+
 function setup() {
   createCanvas(500, 500);
+  f1 = loadFont("assets/oswald.ttf");
+  f2 = loadFont("assets/qwitcherBold.ttf");
+  f3 = loadFont("assets/qwitcherReg.ttf");
+  textAlign(CENTER);
 }
 
 function draw() {
-  
+  background("black");
+  fill("white");
+  textFont(f1, 48);
+  text("hello there!", width/2, 100); 
+  textFont(f3, 48);
+  text("I am Obi Wan", width/2, 200); 
+  textFont(f2, 60);
+  text("Kenobi", width/2, 300); 
 }
