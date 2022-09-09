cad8e09ea167c903773abc31b6697b07a0bff0a4
gabefulk8 <89150841+gabefulk8@users.noreply.github.com>
2022-09-09 15:28:40 -0500

    sound project

--- Unit1/fonts/sketch.js
+++ Unit1/fonts/sketch.js
@@ -12,9 +12,9 @@ function draw() {
   background("black");
   fill("white");
   textFont(f1, 48);
-  text("hello there!", width/2, 100); 
+  text("Ah!", width/2, 100); 
   textFont(f3, 48);
-  text("I am Obi Wan", width/2, 200); 
+  text("General", width/2, 200); 
   textFont(f2, 60);
   text("Kenobi", width/2, 300); 
 }
---
+++ Unit1/sound/sketch.js
@@ -0,0 +1,31 @@
+/// <reference path="../../libs/TSDef/p5.global-mode.d.ts" /> 
+
+let s1;
+
+function preload() {
+  s1 = loadSound("assets/evolution.mp3");
+}
+
+
+function setup() {
+  createCanvas(500, 500);
+  textAlign(CENTER);
+  s1.play();
+
+}
+
+function draw() {
+  background("black");
+  fill("white");
+
+  text("This sound is called Evolution. It gives me 'Space Movie' vibes :)", 100, 100, 200, 200);
+}
+
+
+function mouseReleased() {
+  if (s1.isPlaying() == true) {
+    s1.pause();
+  } else {
+    s1.play();
+  }
+}
