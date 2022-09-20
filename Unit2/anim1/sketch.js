ca869333160d72c325830c01aae474c8988b8b99
gabefulk8 <89150841+gabefulk8@users.noreply.github.com>
2022-09-20 11:39:42 -0500

    anim1, anim2

---
+++ Unit2/anim1/sketch.js
@@ -0,0 +1,74 @@
+/// <reference path="../../libs/TSDef/p5.global-mode.d.ts" /> 
+
+let x = 0;
+let txt = "Sweet Home Alabama";
+let font;
+let state = 0;
+
+function setup() {
+  createCanvas(500, 500);
+  font = loadFont("assets/oswald.ttf");
+}
+
+function draw() {
+  if (state == 0) {
+
+    background(220);
+    textSize(32);
+    fill("black");
+    text(txt, x, 200);
+  
+    x += 2;
+
+    if (x > width) {
+      x = 0; 
+      txt = "Where the skies are so Blue"; 
+      state = 1;
+    }
+  }
+
+  if (state == 1) {
+    background(0, 0, 255);
+    fill("black");
+    text(txt, x, 200);
+  
+    x += 2;
+
+    if (x > width) {
+      x = 0; 
+      txt = "Sweet Home Alabama"; 
+      state = 2;
+    }
+  }
+
+  if (state == 2) {
+
+    background(220);
+    textSize(32);
+    fill("black");
+    text(txt, x, 200);
+  
+    x += 2;
+
+    if (x > width) {
+      x = 0; 
+      txt = "Lord I'm comin' home to you"; 
+      state = 3;
+    }
+  }
+  if (state == 3) {
+
+    background(0, 255, 0);
+    textSize(32);
+    fill("black");
+    text(txt, x, 200);
+  
+    x += 2;
+
+    if (x > width) {
+      x = 0; 
+      txt = "Sweet Home Alabama"; 
+      state = 0;
+    }
+  }
+}
---
+++ Unit2/anim2/sketch.js
@@ -0,0 +1,51 @@
+function setup() {
+  createCanvas(500, 500);
+}
+
+function draw() {
+  avatar();
+}
+
+function avatar() {
+  noStroke();
+
+  //body
+  fill("rgb(110,110,110)");
+  ellipse(250, 250, 80, 180);
+
+  //head
+  fill("rgb(110,110,110)");
+  circle(250, 140, 70);
+  fill("rgb(100, 100, 100)");
+  quad(215, 135, 285, 135, 290, 170, 210, 170);
+
+  fill("rgb(185,185,185)");
+  circle(250, 150, 57);
+
+  fill("rgb(110,110,110)");
+  ellipse(250, 126, 50, 15);
+  ellipse(250, 170, 80, 30);
+
+  fill("rgb(230,230,230)");
+  ellipse(262, 146, 9, 13);
+  ellipse(242, 146, 9, 13);
+
+  fill("rgb(180,29,180)")
+  ellipse(262, 147, 5, 7);
+  ellipse(242, 147, 5, 7);
+
+  //hands
+  fill("rgb(185,185,185)");
+  circle(202, 262, 15);
+  circle(297, 261, 15);
+
+  //arms
+  fill("rgb(100, 100, 100)");
+  triangle(276, 180, 305, 260, 290, 265);
+  triangle(224, 180, 210, 265, 193, 260);
+
+  //feet
+  fill("rgb(90, 90, 90)");
+  ellipse(235, 332, 40, 30);
+  ellipse(265, 332, 40, 30);
+}
